4f2ea0dab9f57e4ee559e416efdc0c7bd45a4fef
Coded Journey <coded.journey@gmail.com>
2020-06-20 15:10:10 -0700

    Added inline styling and reference variable and function

--- src/index.js
+++ src/index.js
@@ -2,9 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// Reference function to get button text
+function getButtonText() {
+  return 'Click on me!';
+}
+
 // Create a react component
 const App = () => {
-  return <div>Hello world!</div>;
+  // variables that store data
+  // const buttonText = 'click here';
+
+  return (
+    <div>
+      <label className='label' for='name'>Enter name:</label>
+      <input id='name' type='text' />
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        {/* You can reference a variable or a function */}
+        {getButtonText()}
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
